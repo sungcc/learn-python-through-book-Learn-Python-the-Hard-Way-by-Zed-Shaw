bfa67044fa0b88c494e867fb9c651f63635972b5
sungcc <samooies@yahoo.com.hk>
2022-12-02 09:27:34 +0800

    update ex13

---
+++ ex13.py
@@ -0,0 +1,17 @@
+# Parameters, Unpacking, Variables
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+
+# this makes me feel the power of python
+# my first script here great
+
+# argument variable, this is really difficult for me, non native English
+# to feel such meaning
+# it's way better now
